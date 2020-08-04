b83c05011d951a89dbcf60f9f9ea6bdf6eee6ac6
Lawrence Gow <lgow123456@googlemail.com>
2020-08-04 14:02:42 +0100

    Updates the release script
    - Moves the building to after the update of the version.
    - Copies the build to the working directory to avoid extra folders
    in the generated archive.

--- _CreateReleasePackage.ps1
+++ _CreateReleasePackage.ps1
@@ -1,8 +1,5 @@
 $version = Read-Host 'New Version'
 
-# Build the project in Release
-&"D:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\MSBuild.exe" "GW2BuildLibrary.sln" /t:Clean,Build /p:Configuration=Release /p:Platform="Any CPU"
-
 # Update AssemblyVersion in the project
 $infoPath = "Properties\AssemblyInfo.cs"
 $regex = '^\[assembly: AssemblyVersion\(".*"\)\]$'
@@ -14,6 +11,9 @@ $infoPath = "Properties\AssemblyInfo.cs"
 $regex = '^\[assembly: AssemblyFileVersion\(".*"\)\]$'
 $newVersion = '[assembly: AssemblyFileVersion("' + $version + '")]'
 (Get-Content $infoPath) -replace $regex, $newVersion | Set-Content $infoPath
+
+# Build the project in Release
+&"D:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\MSBuild.exe" "GW2BuildLibrary.sln" /t:Clean,Build /p:Configuration=Release /p:Platform="Any CPU"
 
 $arr = $version.Split(".")
 $tag = "INVALID"
@@ -39,11 +39,18 @@ if ($tag -eq "INVALID") {
 	write-host -f green "HEAD tagged with: '$version'"
 	
 	# Create the zip file containing the built release
-	$exeLocation = "bin\Release\GW2BuildLibrary.exe"
-	$readmeLocation = "bin\Release\README.pdf"
+	# Files to include
+	$file1 = "GW2BuildLibrary.exe"
+	$file2 = "README.pdf"
+	$file3 = "GW2BuildLibrary.exe.config"
+	
 	$archiveName = "GW2BuildLibrary_$version.zip"
 
-	&"D:\Program Files (x86)\7-Zip\7z.exe" a -tzip $archiveName $exeLocation $readmeLocation
+	# Copy files into current directory
+	xcopy bin\Release\* . /Y
+	
+	# Create the archive
+	&"D:\Program Files (x86)\7-Zip\7z.exe" a -tzip $archiveName $file1 $file2 $file3
 	write-host -f green "Release archive created."
 	
 	# Undo file changes
