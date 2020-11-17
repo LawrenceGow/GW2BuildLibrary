d973403fa6bf08ffdab264055a1c0afa126210c8
Lawrence Gow <lgow123456@googlemail.com>
2020-11-17 23:44:49 +0000

    Adds Method of Generating Specialisation Enum

--- BuildLibrary/TemplateHelper.cs
+++ BuildLibrary/TemplateHelper.cs
@@ -1,4 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
 
 namespace GW2BuildLibrary
 {
@@ -22,7 +26,7 @@ namespace GW2BuildLibrary
         { };
 
         /// <summary>
-        /// Gets the byte that represents the set of elite specs required
+        /// Gets the byte that represents the set of elite specialisations required
         /// for the specializations provided.
         /// </summary>
         /// <param name="eliteSlotByte">The elite specialization slot byte.</param>
@@ -68,6 +72,52 @@ namespace GW2BuildLibrary
         public static bool IsBasedOn(this Profession profession, in Profession check)
         {
             return ((byte)profession & 0b00001111) == (byte)check;
+        }
+
+        /// <summary>
+        /// Writes a file contain the currently available specialisations for all classes.
+        /// </summary>
+        public static async void WriteSpecialisationsFile()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                // Get the list of specialisation ids
+                string specIds = string.Join(",",
+                    JArray.Parse(await client.GetStringAsync("https://api.guildwars2.com/v2/specializations")));
+
+                // Get the specialisation info
+                JArray specs = JArray.Parse(await client.GetStringAsync($"https://api.guildwars2.com/v2/specializations?ids={specIds}"));
+
+                using (StreamWriter file = new StreamWriter(Path.Combine(App.BaseDirectory, "Specialization.txt"), false))
+                {
+                    WriteSpecialisations("Guardian", specs, file);
+                    WriteSpecialisations("Warrior", specs, file);
+                    WriteSpecialisations("Engineer", specs, file);
+                    WriteSpecialisations("Ranger", specs, file);
+                    WriteSpecialisations("Thief", specs, file);
+                    WriteSpecialisations("Elementalist", specs, file);
+                    WriteSpecialisations("Mesmer", specs, file);
+                    WriteSpecialisations("Necromancer", specs, file);
+                    WriteSpecialisations("Revenant", specs, file);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Writes the specialisation enum values with the given <see cref="StreamWriter"/>.
+        /// </summary>
+        /// <param name="professionName">The name of the profession to write about.</param>
+        /// <param name="specs">The collection of specialisation JSON objects.</param>
+        /// <param name="file">The <see cref="StreamWriter"/> to write to.</param>
+        private static void WriteSpecialisations(string professionName, JArray specs, StreamWriter file)
+        {
+            file.WriteLine($"// {professionName}");
+            foreach (JToken spec in specs.Where(s => s["profession"].ToString() == professionName))
+            {
+                string name = spec["name"].ToString().Replace(" ", "");
+                file.WriteLine($"{name} = {spec["id"]},");
+            }
+            file.WriteLine();
         }
     }
 }
