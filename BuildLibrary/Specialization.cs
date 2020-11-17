16b93811e463d5ca6846ce3f5a7ba4fa1b0f0425
Lawrence Gow <lgow123456@googlemail.com>
2020-11-17 23:45:29 +0000

    Fills out the Specialisation Enum

--- BuildLibrary/Specialization.cs
+++ BuildLibrary/Specialization.cs
@@ -1,30 +1,91 @@
 ﻿namespace GW2BuildLibrary
 {
-	// TODO Write a small application to create this enum via the API
-	// would also be good for the Professions enum too
-	// could make getting/updating the images easier too
+    /// <summary>
+    /// The available specialisations.
+    /// </summary>
     public enum Specialization : byte
     {
         None = 0,
 
         // Guardian
+        Valor = 13,
+        Radiance = 16,
+        Dragonhunter = 27,
+        Zeal = 42,
+        Virtues = 46,
+        Honor = 49,
+        Firebrand = 62,
 
         // Warrior
+        Strength = 4,
+        Tactics = 11,
+        Berserker = 18,
+        Defense = 22,
+        Arms = 36,
+        Discipline = 51,
+        Spellbreaker = 61,
 
         // Engineer
+        Explosives = 6,
+        Tools = 21,
+        Alchemy = 29,
+        Firearms = 38,
+        Scrapper = 43,
+        Inventions = 47,
+        Holosmith = 57,
 
         // Ranger
+        Druid = 5,
+        Marksmanship = 8,
+        NatureMagic = 25,
+        Skirmishing = 30,
+        Beastmastery = 32,
+        WildernessSurvival = 33,
+        Soulbeast = 55,
 
         // Thief
+        Daredevil = 7,
+        ShadowArts = 20,
+        DeadlyArts = 28,
+        CriticalStrikes = 35,
+        Trickery = 44,
+        Acrobatics = 54,
+        Deadeye = 58,
 
         // Elementalist
+        Water = 17,
+        Earth = 26,
+        Fire = 31,
+        Arcane = 37,
+        Air = 41,
+        Tempest = 48,
+        Weaver = 56,
 
         // Mesmer
         Dueling = 1,
+        Domination = 10,
+        Inspiration = 23,
+        Illusions = 24,
+        Chronomancer = 40,
+        Chaos = 45,
+        Mirage = 59,
 
         // Necromancer
         DeathMagic = 2,
+        BloodMagic = 19,
+        Reaper = 34,
+        Curses = 39,
+        SoulReaping = 50,
+        Spite = 53,
+        Scourge = 60,
 
         // Revenant
+        Invocation = 3,
+        Retribution = 9,
+        Salvation = 12,
+        Corruption = 14,
+        Devastation = 15,
+        Herald = 52,
+        Renegade = 63,
     }
 }
