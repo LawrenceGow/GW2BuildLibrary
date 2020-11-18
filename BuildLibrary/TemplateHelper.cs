cdc04c23db30f28c8d06dbc57cad9763a4ef9b34
Lawrence Gow <lgow123456@googlemail.com>
2020-11-18 14:16:50 +0000

    Cleans TemplateHelper.cs

--- BuildLibrary/TemplateHelper.cs
+++ BuildLibrary/TemplateHelper.cs
@@ -1,9 +1,4 @@
-﻿using Newtonsoft.Json.Linq;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Net.Http;
-using System.Windows;
+﻿using System.Collections.Generic;
 
 namespace GW2BuildLibrary
 {
