d8308f2997d321f6f2e8ed1f13a66f2e70896f00
Lawrence Gow <lgow123456@googlemail.com>
2020-11-18 20:57:07 +0000

    Generate Profession Icons
    No longer have spearate calls to get the specialisation or profession ids, we
    can just use 'all' for the `ids` param in the request.
    Now save the profession icons, including the icon for elites.

--- ApiDataGenerator/Program.cs
+++ ApiDataGenerator/Program.cs
@@ -11,35 +11,31 @@ namespace ApiDataGenerator
     internal class Program
     {
         private const string apiURL = @"https://api.guildwars2.com/v2";
-        private static readonly string apiOutputPath = Path.Combine(@"D:\Temp\GW2BuildLibrary");
-        private static readonly string specsFilePath = Path.Combine(apiOutputPath, "Specialization.txt");
+        private static readonly string apiOutputDir = Path.Combine(@"D:\Temp\GW2BuildLibrary");
+        private static readonly string specsFilePath = Path.Combine(apiOutputDir, "Specialization.txt");
+        private static readonly string specIconsDir = Path.Combine(apiOutputDir, @"Icons\Specialisations");
+        private static readonly string professionIconsDir = Path.Combine(apiOutputDir, @"Icons\Professions");
 
         private static async Task Main(string[] args)
         {
+            Directory.CreateDirectory(apiOutputDir);
+            Directory.CreateDirectory(specIconsDir);
+            Directory.CreateDirectory(professionIconsDir);
+
             using (HttpClient client = new HttpClient())
             {
-                // Get the list of professions ids
-                string professionIds = string.Join(",",
-                    JArray.Parse(await client.GetStringAsync($"{apiURL}/professions")));
+                // Get the professions
+                JArray professions = JArray.Parse(await client.GetStringAsync($"{apiURL}/professions?ids=all"));
+                await SaveProfessionIcons(professions);
 
-                // Get the profession info
-                JArray professions = JArray.Parse(await client.GetStringAsync($"{apiURL}/professions?ids={professionIds}"));
-
-                // Get the list of specialisation ids
-                string specIds = string.Join(",",
-                    JArray.Parse(await client.GetStringAsync($"{apiURL}/specializations")));
-
-                // Get the specialisation info
-                JArray specs = JArray.Parse(await client.GetStringAsync($"{apiURL}/specializations?ids={specIds}"));
-
-                // Ensure the output directory exists
-                Directory.CreateDirectory(apiOutputPath);
+                // Get the specialisations
+                JArray specs = JArray.Parse(await client.GetStringAsync($"{apiURL}/specializations?ids=all"));
 
                 File.Delete(specsFilePath);
                 using (StreamWriter specFile = new StreamWriter(specsFilePath, false))
                 {
-                    foreach (var profession in professions)
-                        WriteSpecialisationsForProfession(profession["name"].ToString(), specs, specFile);
+                    foreach (JToken profession in professions)
+                        await WriteSpecialisationsForProfession(profession["name"].ToString(), specs, specFile);
                 }
             }
 
@@ -47,7 +43,21 @@ namespace ApiDataGenerator
             Console.WriteLine("DONE! Press any key to exit.");
             Console.ReadKey();
             // Open the output folder
-            Process.Start(apiOutputPath);
+            Process.Start(apiOutputDir);
+        }
+
+        private static async Task SaveProfessionIcons(JArray professions)
+        {
+            foreach (JToken profession in professions)
+            {
+
+                using (HttpClient client = new HttpClient())
+                {
+                    string iconURL = profession["icon"].ToString();
+                    WriteStreamToFile(await client.GetStreamAsync(iconURL),
+                        Path.Combine(professionIconsDir, $"{profession["name"]}.png"));
+                }
+            }
         }
 
         /// <summary>
@@ -57,37 +67,47 @@ namespace ApiDataGenerator
         /// <param name="professionName">The name of the profession to write about.</param>
         /// <param name="specs">The collection of specialisation JSON objects.</param>
         /// <param name="specFile">The <see cref="StreamWriter"/> to write to.</param>
-        private static void WriteSpecialisationsForProfession(string professionName, JArray specs, StreamWriter specFile)
+        private static async Task WriteSpecialisationsForProfession(string professionName, JArray specs, StreamWriter specFile)
         {
             specFile.WriteLine($"// {professionName}");
             foreach (JToken spec in specs.Where(s => s["profession"].ToString() == professionName))
             {
                 string specName = spec["name"].ToString().Replace(" ", "");
                 specFile.WriteLine($"{specName} = {spec["id"]},");
-                WriteSpecialisationImgFile(specName, spec);
+                await SaveSpecialisationImage(specName, spec);
             }
             specFile.WriteLine();
         }
 
         /// <summary>
-        /// Writes the specialisation icon to a file.
+        /// Saves the specialisation icon.
         /// </summary>
         /// <param name="specName">The name of the specialisation.</param>
         /// <param name="spec">The specialisation JSON object.</param>
-        private static async void WriteSpecialisationImgFile(string specName, JToken spec)
+        private static async Task SaveSpecialisationImage(string specName, JToken spec)
         {
-            string dir = Path.Combine(apiOutputPath, @"Icons\Specialisations");
-            Directory.CreateDirectory(dir);
-
-            using (FileStream specImgFile = new FileStream(Path.Combine(dir, $"{specName}.png"),
-                FileMode.OpenOrCreate, FileAccess.ReadWrite))
             using (HttpClient client = new HttpClient())
             {
                 string iconURL = spec["icon"].ToString();
-                Stream data = await client.GetStreamAsync(iconURL);
+                WriteStreamToFile(await client.GetStreamAsync(iconURL),
+                    Path.Combine(specIconsDir, $"{specName}.png"));
+
+                JToken profIcon = spec["profession_icon"];
+                if (profIcon != null)
+                {
+                    WriteStreamToFile(await client.GetStreamAsync(profIcon.ToString()),
+                        Path.Combine(professionIconsDir, $"{specName}.png"));
+                }
+            }
+        }
+
+        private static void WriteStreamToFile(Stream source, string path)
+        {
+            using (FileStream file = new FileStream(path, FileMode.OpenOrCreate, FileAccess.ReadWrite))
+            {
                 int b;
-                while ((b = data.ReadByte()) != -1)
-                    specImgFile.WriteByte((byte)b);
+                while ((b = source.ReadByte()) != -1)
+                    file.WriteByte((byte)b);
             }
         }
     }
