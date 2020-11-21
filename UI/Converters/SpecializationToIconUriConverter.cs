b4fdca93086821081d8df7fada4bea141ad58691
Lawrence Gow <lgow123456@googlemail.com>
2020-11-21 13:31:12 +0000

    Adds SpecializationToIconUriConverter

---
+++ UI/Converters/SpecializationToIconUriConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace GW2BuildLibrary.UI.Converters
+{
+    /// <summary>
+    /// Converter used to convert a given <see cref="Specialization"/> member into its icon location.
+    /// </summary>
+    public class SpecializationToIconUriConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Specialization specialization = (Specialization)value;
+
+            // Show no icon for a blank profession
+            if (specialization == Specialization.None)
+                return null;
+
+            return $"pack://application:,,,/Icons/{specialization}_Specialization.png";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
+            throw new NotImplementedException();
+    }
+}
