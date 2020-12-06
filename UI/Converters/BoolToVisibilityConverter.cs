b0ebf46bba0fd7b4563c9554fa443f48137d3152
Lawrence Gow <lgow123456@googlemail.com>
2020-12-06 14:18:39 +0000

    Comments for all converters

--- UI/Converters/BoolToVisibilityConverter.cs
+++ UI/Converters/BoolToVisibilityConverter.cs
@@ -13,6 +13,14 @@ namespace GW2BuildLibrary.UI.Converters
     {
         #region Methods
 
+        /// <summary>
+        /// Converts a value.
+        /// </summary>
+        /// <param name="value">The boolean value produced by the binding source.</param>
+        /// <param name="targetType">The type of the binding target property.</param>
+        /// <param name="parameter">The converter parameter to use.</param>
+        /// <param name="culture">The culture to use in the converter.</param>
+        /// <returns>An input of <c>true</c> produces <c>Visible</c>, <c>false</c> produces <c>Collapsed</c>.</returns>
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             bool boolValue = (bool)value;
@@ -20,6 +28,14 @@ namespace GW2BuildLibrary.UI.Converters
             return boolValue ? Visibility.Visible : Visibility.Collapsed;
         }
 
+        /// <summary>
+        /// Converts a value.
+        /// </summary>
+        /// <param name="value">The value that is produced by the binding target.</param>
+        /// <param name="targetType">The type to convert to.</param>
+        /// <param name="parameter">The type to convert to.</param>
+        /// <param name="culture">The culture to use in the converter.</param>
+        /// <returns>Nothing.</returns>
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
             throw new NotImplementedException();
 
--- UI/Converters/SpecializationToIconUriConverter.cs
+++ UI/Converters/SpecializationToIconUriConverter.cs
@@ -11,6 +11,14 @@ namespace GW2BuildLibrary.UI.Converters
     {
         #region Methods
 
+        /// <summary>
+        /// Converts a value.
+        /// </summary>
+        /// <param name="value">The boolean value produced by the binding source.</param>
+        /// <param name="targetType">The type of the binding target property.</param>
+        /// <param name="parameter">The converter parameter to use.</param>
+        /// <param name="culture">The culture to use in the converter.</param>
+        /// <returns>An Uri to the icon for the input <see cref="Specialization"/>.</returns>
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             Specialization specialization = (Specialization)value;
@@ -22,6 +30,14 @@ namespace GW2BuildLibrary.UI.Converters
             return $"pack://application:,,,/Icons/{specialization}_Specialization.png";
         }
 
+        /// <summary>
+        /// Converts a value.
+        /// </summary>
+        /// <param name="value">The value that is produced by the binding target.</param>
+        /// <param name="targetType">The type to convert to.</param>
+        /// <param name="parameter">The type to convert to.</param>
+        /// <param name="culture">The culture to use in the converter.</param>
+        /// <returns>Nothing.</returns>
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
             throw new NotImplementedException();
 
